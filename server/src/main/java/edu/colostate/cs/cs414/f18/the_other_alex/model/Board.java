47c1b5e02f928fe96b4635011f5e5f879d38066a
ajthomps <ajthomps@rams.colostate.edu>
2018-10-28 15:04:15 -0600

    still need to implement anyPossibleMovesLeft and isGameOver()

--- server/src/main/java/edu/colostate/cs/cs414/f18/the_other_alex/model/Board.java
+++ server/src/main/java/edu/colostate/cs/cs414/f18/the_other_alex/model/Board.java
@@ -1,11 +1,63 @@
 package edu.colostate.cs.cs414.f18.the_other_alex.model;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class Board {
   private Cell[][] cells;
+  private List<Piece> pieces;
 
-  public void move(Cell fromCell, Cell toCell) {
-    //check to make sure piece in fromCell are different from piece in toCell
+  public Board() {
+    cells = new Cell[4][8];
+    pieces = new ArrayList<Piece>();
+    loadPieces();
+    loadCells();
+  }
 
+  //randomizes placement of pieces onto 4x8 cells array
+  private void loadCells() {
+    Random randomGenerator = new Random();
+    for (int i = 0; i < 4; i++)
+      for (int j = 0; j < 8; j++) {
+        int randomIndex = randomGenerator.nextInt(pieces.size());
+        Piece piece = pieces.remove(randomIndex);
+        cells[i][j] = new Cell(i, j, piece);
+      }
+  }
+
+  //creates list of 16 black and 16 red players (still needs the color argument for piece constructor)
+  private void loadPieces() {
+    pieces.add(new General(PieceColor.BLACK));
+    pieces.add(new General(PieceColor.WHITE));
+    for (int i = 0; i < 2; i++) {
+      pieces.add(new Advisor(PieceColor.BLACK));
+      pieces.add(new Advisor(PieceColor.WHITE));
+      pieces.add(new Elephant(PieceColor.BLACK));
+      pieces.add(new Elephant(PieceColor.WHITE));
+      pieces.add(new Chariot(PieceColor.BLACK));
+      pieces.add(new Chariot(PieceColor.WHITE));
+      pieces.add(new Horse(PieceColor.BLACK));
+      pieces.add(new Horse(PieceColor.WHITE));
+      pieces.add(new Cannon(PieceColor.BLACK));
+      pieces.add(new Cannon(PieceColor.WHITE));
+    }
+    for (int i = 0; i < 5; i ++) {
+      pieces.add(new Soldier(PieceColor.BLACK));
+      pieces.add(new Soldier(PieceColor.WHITE));
+    }
+  }
+
+  public void move(Cell fromCell, Cell toCell) throws InvalidMoveException {
+    boolean moveValid = fromCell.getPiece().isMoveValid(fromCell, toCell, cells);
+    if (moveValid) {
+      Piece fromPiece = fromCell.getPiece();
+      toCell.setPiece(fromPiece);
+      fromCell.setPiece(new NullPiece());
+    }
+    else {
+      throw new InvalidMoveException("Invalid move. Select a different move");
+    }
   }
 
   public boolean anyPossibleMovesLeft() {
@@ -13,6 +65,8 @@ public class Board {
   }
 
   public boolean isGameOver() {
+
     return false;
   }
+
 }
