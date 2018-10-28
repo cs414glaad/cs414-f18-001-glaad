033a0bad553612e26e4f3ea05c77a3aa001d54ff
Lucas Wilson <lkwilson96@gmail.com>
2018-10-28 17:51:10 -0600

    Develop API data and add tests

--- server/src/main/java/edu/colostate/cs/cs414/f18/the_other_alex/model/User.java
+++ server/src/main/java/edu/colostate/cs/cs414/f18/the_other_alex/model/User.java
@@ -1,12 +1,17 @@
 package edu.colostate.cs.cs414.f18.the_other_alex.model;
 
+import java.util.ArrayList;
+import java.util.BitSet;
+
 public class User {
   private String name;
+  private String username;
   private String email;
   private String password;
-  private Invite[] pendingInvites;
-  private Invite[] pendingReceivedInvites;
+  private ArrayList<Invite> pendingInvites;
+  private ArrayList<Invite> pendingReceivedInvites;
   private UserHistory userHistory;
+  private ArrayList<String> games;
 
   public Invite sendInvite(String username) {
     return null;
@@ -22,5 +27,21 @@ public class User {
 
   public UserHistory getUserHistory() {
     return userHistory;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public ArrayList<Invite> getInvites() {
+    return pendingInvites;
+  }
+
+  public ArrayList<String> getGames() {
+    return games;
   }
 }
---
+++ server/src/main/java/edu/colostate/cs/cs414/f18/the_other_alex/server/resjson/PieceData.java
@@ -0,0 +1,27 @@
+package edu.colostate.cs.cs414.f18.the_other_alex.server.resjson;
+
+import edu.colostate.cs.cs414.f18.the_other_alex.model.Cell;
+import edu.colostate.cs.cs414.f18.the_other_alex.model.Piece;
+import edu.colostate.cs.cs414.f18.the_other_alex.server.DataType;
+
+public class PieceData extends DataType {
+  // row and column are stored in id
+  public String color;
+  public boolean isFlipped;
+
+  public PieceData(Piece piece) {
+    switch(piece.getColor()) {
+      case NONE:
+        color = null;
+      case BLACK:
+        color = "black";
+      case RED:
+        color = "red";
+    }
+    isFlipped = piece.getIsFlipped();
+  }
+
+  public PieceData(Cell cell) {
+    this(cell.getPiece());
+  }
+}
